52a53c5ac1e803645bd002cda7443ed0df199860
cheonTH <xogus0530@gmail.com>
2025-08-13 22:45:03 +0900

    냉장고 배경색 수정

--- src/components/fridge/RefrigeratorPage.js
+++ src/components/fridge/RefrigeratorPage.js
@@ -35,7 +35,7 @@ const RefrigeratorPage = ({ loggedInUserId, loggedInNickName }) => { // isLoadin
     }
     
     return (
-        <div className="min-h-screen bg-gradient-to-br from-blue-100 to-indigo-200 flex items-center justify-center p-4">
+        <div className="min-h-screen bg-gradient-to-br from-gray-100 to-gray-100 flex items-center justify-center p-4" style={{border: "1px, solid, eee", borderRadius: "8px"}}>
             
             <div className="flex w-full max-w-7xl h-[800px] bg-white rounded-2xl shadow-2xl overflow-hidden">
                 <Sidebar
